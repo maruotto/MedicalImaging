ffb8aa51a18636d97b42d6f1811e1a28f005cd17
maruotto <94022256+maruotto@users.noreply.github.com>
2022-11-21 12:13:45 +0100

    Cose prima di fare altro

--- filtering.py
+++ filtering.py
@@ -3,32 +3,36 @@ from image import normalize, show_img
 import unicodedata
 from tkinter import Tk as tk
 from tkinter.filedialog import askopenfilename
-
-#import numpy as np
+import matplotlib.pyplot as plt
 import cv2
 
 def img_path_read():
     tk().withdraw()
-    img_path = askopenfilename(message="Choose a file to open", initialdir=r"/Users/idamaruotto/Downloads/Segmentaziones/dataset/train/")
+    img_path = askopenfilename(message="Choose a file to open", initialdir=r"/Users/idamaruotto/Downloads/Mammotest")
     print(img_path)
     #uni_code = easygui.fileopenbox(msg="Choose a file to open",
-    #                               default=r"/Users/idamaruotto/Downloads/Segmentaziones/dataset/train/")
+    #                               default=r"/Users/idamaruotto/Downloads/Mammotest")
     img_path = unicodedata.normalize('NFKD', img_path).encode('ascii', 'ignore')
     img_path = img_path.decode('utf-8')
     #img_path = "/Users/idamaruotto/Downloads/Segmentaziones/dataset/train/00001_p0_patch0.tif"
     return img_path
 
+
 def main():
     img_path = img_path_read()
-    src = cv2.imread(img_path,0)
-    #src = normalize(src)
-    print("coso")
-    #cv2.imshow('name', src)
-    show_img(src)
-    cv2.waitKey()
-
-    #dst = cv2.LUT(src.astype(np.uint8), lut)
-    #show_img(dst, 'Gamma corrected image')
+    img = cv2.imread(img_path,0)
+    laplacian = cv2.Laplacian(img,cv2.CV_64F)
+    sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+    sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+    plt.subplot(2,2,1),plt.imshow(img,cmap ='Oranges')
+    plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+    plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+    plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+    plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+    plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+    plt.show()
 
 
 if __name__ == '__main__':
